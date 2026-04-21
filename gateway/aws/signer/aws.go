e5e3f348aea3cf7c115e217ee683de04333bc17f
Jonathan Schwarze <jonathan.schwarze@sap.com>
2026-04-21 09:50:16 +0200

    fix: correct nolint comment alignment for goimports
    Signed-off-by: Jonathan Schwarze <jonathan.schwarze@sap.com>

--- gateway/aws/signer/aws.go
+++ gateway/aws/signer/aws.go
@@ -17,8 +17,8 @@ import (
 	"opensearch-cli/entity"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws/credentials" //nolint:staticcheck // TODO: migrate to aws-sdk-go-v2
-	"github.com/aws/aws-sdk-go/aws/session"     //nolint:staticcheck // TODO: migrate to aws-sdk-go-v2
+	"github.com/aws/aws-sdk-go/aws/credentials"  //nolint:staticcheck // TODO: migrate to aws-sdk-go-v2
+	"github.com/aws/aws-sdk-go/aws/session"      //nolint:staticcheck // TODO: migrate to aws-sdk-go-v2
 	v4 "github.com/aws/aws-sdk-go/aws/signer/v4" //nolint:staticcheck // TODO: migrate to aws-sdk-go-v2
 	"github.com/hashicorp/go-retryablehttp"
 )
